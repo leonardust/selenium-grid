f35ee411a15927865bfd04a7534937ce6577dbe9
Lukasz Komorowski <lukasz.komorowski.edu@gmail.com>
2023-08-23 09:20:45 +0200

    init

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 This is a base code, that we use to start Selenium grid and cloud course
 
 If you are new to Selenium with Java and TestNG, I suggest looking at my courses, and learn how to build project like this here: https://practicetestautomation.com/courses/
+
+## Selenium Grid components
+![img.png](img.png)
+
+## Running grid
+
+Download selenium server(grid) from https://www.selenium.dev/downloads/
+1. Start the `Event Bus` 
+```bash
+java -jar selenium-server-4.11.0.jar event-bus
+```
+2. Start the `Session Map`
+```bash
+java -jar selenium-server-4.11.0.jar sessions
+```
+3. Start the new `session queue`
+```bash
+java -jar selenium-server-4.11.0.jar sessionqueue
+```
+4. Start the `Distributor`
+```bash
+java -jar selenium-server-4.11.0.jar distributor --sessions http://localhost:5556 --sessionqueue http://localhost:5559 --bind-bus false 
+```
+5. Start the `Router`
+```bash
+java -jar selenium-server-4.11.0.jar router --sessions http://localhost:5556 --distributor http://localhost:5553 --sessionqueue http://localhost:5559
+```
+6. Add a `Node`
+```bash
+java -jar selenium-server-4.11.0.jar node --detect-drivers true
+```
--- src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
+++ src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
@@ -3,16 +3,21 @@ package com.practicetestautomation.base;
 import java.util.logging.Level;
 
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserDriverFactory {
 
-	private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	private String browser;
-	private Logger log;
+	private final String browser;
+	private final Logger log;
 
 	public BrowserDriverFactory(String browser, Logger log) {
 		this.browser = browser.toLowerCase();
@@ -21,32 +26,22 @@ public class BrowserDriverFactory {
 
 
 	public WebDriver createDriver() {
+  		RemoteWebDriver driver;
 		log.info("Create driver: " + browser);
 
 		switch (browser) {
-		case "chrome":
-			// Make sure to upgrade chromedriver to work with your browser version: https://chromedriver.chromium.org/downloads
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-			driver.set(new ChromeDriver());
-			break;
-
-		case "firefox":
-			// Make sure to upgrade geckodriver to work with your browser version: https://github.com/mozilla/geckodriver/releases
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-			System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
-			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
-			driver.set(new FirefoxDriver());
-			break;
-
-		default:
-			log.debug("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-			driver.set(new ChromeDriver());
-			break;
+			case "chrome" -> driver = new ChromeDriver();
+			case "firefox" -> driver = new FirefoxDriver();
+			case "edge" -> driver = new EdgeDriver();
+			default -> throw new IllegalArgumentException("Invalid browser value: " + browser);
 		}
-		java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
-		return driver.get();
-	}
+
+		Capabilities caps = driver.getCapabilities();
+		String browserName = caps.getBrowserName();
+		String browserVersion = caps.getBrowserVersion();
+		log.info("Browser: " + browserName);
+		log.info("Version: " + browserVersion);
+
+		return driver;
+    }
 }
